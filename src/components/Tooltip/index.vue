ee20e4da7f948c1ec232f18dfad3e818efc59bff
Lasse <lasse.j.lundin@gmail.com>
2021-01-26 10:27:26 +0100

    Created Tooltip component

---
+++ src/components/Tooltip/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "Tooltip",
+  props: {
+    active: {
+      type: Boolean,
+      default: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
